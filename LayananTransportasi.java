8f328339e2c0b372b03f03a10453b935b038cdcb
nadhifiatalitha <nadhifiatalitha@gmail.com>
2026-04-14 14:52:39 +0700

    menambahkan class madiunmobil dengan logika nim

--- LayananTransportasi.java
+++ LayananTransportasi.java
@@ -58,3 +58,34 @@ class MadiunMotor extends LayananTransportasi {
         }
     }
 }
+class MadiunMobil extends LayananTransportasi {
+    private int nimAkhir = 3; 
+
+    public MadiunMobil(String namaDriver, double jarak, double saldoUser, int nimAkhir) {
+        super(namaDriver, jarak, saldoUser);
+        this.nimAkhir = nimAkhir;
+    }
+
+    @Override
+    public double hitungTarif() {
+        double biayaAdmin = 3000 + (nimAkhir * 100); 
+        return (jarak * 5000.0) + biayaAdmin;
+    }
+
+    public void prosesTransaksi() {
+        String namaLengkap = "Nadhifiatalitha";
+        String nim = "254311003";
+
+        System.out.println("=== Transaksi oleh " + namaLengkap + " - " + nim + " ===");
+        double totalBayar = hitungTarif();
+        if (isSaldoCukup(totalBayar)) {
+            saldoUser -= totalBayar;
+            System.out.println("Driver: " + namaDriver);
+            System.out.println("Jarak: " + jarak + " km");
+            System.out.println("Total Bayar: Rp " + totalBayar);
+            System.out.println("Sisa Saldo: Rp " + saldoUser);
+        } else {
+            System.out.println("Gagal: Saldo tidak mencukupi untuk perjalanan ini!");
+        }
+    }
+}
